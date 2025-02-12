696a09fa15cee3038316cecb3aae624238b16333
taboorech <karuselpopka@gmail.com>
2025-02-12 13:17:02 +0200

    fix: Update error handler middleware for logging

--- src/middleware/error-handler.ts
+++ src/middleware/error-handler.ts
@@ -1,9 +1,10 @@
 import { NextFunction, Request, Response } from 'express';
 import { CustomError } from '../libs/classes/CustomError.class';
 import { ValidationError } from 'yup';
+import { logger } from '../config/logger';
 
 export const errorHandler = (err: CustomError, req: Request, res: Response, next: NextFunction) => {
-  console.error(err);
+  logger.error(err);
 
   if(err instanceof ValidationError) {
     res.status(400).send({ errors: [{ message: err.errors }] });
